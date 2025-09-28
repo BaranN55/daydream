37dcebace90d5e197e7513c4eb33dd549f2e9bd5
Sergeis1 <ssergeiss46@gmail.com>
2025-09-27 22:52:34 -0400

    Basic Character 1

--- main_meu.gd
+++ main_meu.gd
@@ -12,12 +12,12 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
-	print("start button")
+	get_tree().change_scene_to_file("res://chapter_options.tscn")
 
 
 
 func _on_options_pressed() -> void:
-	get_tree().change_scene_to_file("res://chapter_options.tscn")
+	pass
 	
 	
 
