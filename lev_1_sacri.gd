86ef655b4f62989ed1b12a52c15a71e55afbad58
BaranN55 <Baran.naji89@gmail.com>
2025-09-28 11:28:35 -0400

    Update

--- lev_1_sacri.gd
+++ lev_1_sacri.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 
 func _on_sacri_pressed() -> void:
-	get_tree().change_scene_to_file("res://chapters/chap1lev2.tscn")
+	get_tree().change_scene_to_file("res://chapters/chap_1_lev_2.tscn")
 
 
 func _on_skip_pressed() -> void:
-	get_tree().change_scene_to_file("res://chapters/chap1lev2.tscn")
+	get_tree().change_scene_to_file("res://chapters/chap_1_lev_2.tscn")
